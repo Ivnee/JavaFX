e0138e57663b168555701f36b5cecea5035194f1
Ivan Ognev <Ognev-dmitrov@mail.ru>
2020-03-21 14:15:25 +0300

    ChatPM

---
+++ server/Server.java
@@ -0,0 +1,72 @@
+package server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Vector;
+
+
+public class Server {
+    private Vector<ClientHandler> clients;
+    private AuthService authService;
+
+    public AuthService getAuthService() {
+        return authService;
+    }
+
+    public Server() {
+        clients = new Vector<>();
+        authService = new SimpleAuthService();
+        ServerSocket server = null;
+        Socket socket = null;
+
+        try {
+            server = new ServerSocket(8189);
+            System.out.println("Сервер запущен");
+
+            while (true) {
+                socket = server.accept();
+                System.out.println("Клиент подключился");
+
+                new ClientHandler(socket, this);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                server.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void broadcastMsg(String msg, String nick) {
+        for (ClientHandler c : clients) {
+            c.sendMsg(nick + ": " + msg);
+        }
+    }
+    public void sendPM(ClientHandler clientHandler, String name, String msg){
+        for(ClientHandler ch: clients){
+            if (ch.getNick().equals(name)){
+                ch.sendMsg(clientHandler.getNick()+ " :" + msg);
+                clientHandler.sendMsg(clientHandler.getNick()+ " :" + msg);
+            }
+        }
+    }
+
+    public void subscribe(ClientHandler clientHandler) {
+        clients.add(clientHandler);
+    }
+
+    public void unsubscribe(ClientHandler clientHandler) {
+        clients.remove(clientHandler);
+    }
+
+}
